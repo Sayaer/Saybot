fe4d55eef96bdde84c7a3f588470025a41c14ac3
Brandon Lacasse <blacassedev@gmail.com>
2020-01-31 21:20:49 -0500

    Saybot.py pared down to base functionality. dragalia.py cleaned up a bit. Added loguru to handle logging for sanity reasons. Cleaned up some imports around the project as a whole.

--- Cogs/DragaliaCog/dragalia.py
+++ Cogs/DragaliaCog/dragalia.py
@@ -1,39 +1,42 @@
 import discord
-import requests
-from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from loguru import logger
 from discord.ext.commands import Bot
 from discord.ext import commands
 
-URL = 'https://dragalialost.gamepedia.com/Adventurer_Grid'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup.prettify())
-# pull relevant dragalia pages once, store / iterate / work with them from there
-# chara command needs to be modified so the links are dynamically created (may be better?)
+base_url = 'https://dragalialost.gamepedia.com/'
 
 
 def setup(bot):
     bot.add_cog(Dragalia(bot))
     print('adding the Dragalia cog')
 
-
-
+#TODO: This is intended to validate a URL so we don't send something broken or malicious, need to define further
+def url_validator(url):
+    try:
+        result = urlparse(url)
+        return logger.info(all([result.scheme, result.netloc, result.path]))
+    except:
+        return False
 
 
 class Dragalia(commands.Cog):
     """
     This cog contains commands for Dragalia Lost.
+
+    Currently using gamepedia as a reference.
     """
+
     def __init__(self, bot: Bot):
         self.bot = bot
 
     @commands.command(name='chara', help='Look up character info')
     async def chara(self, ctx, character_name):
-        print('looking it up!')
-        character_page = soup.find('a', text=character_name)
-        if character_page is not None:
-            print(f'Found {character_page}')
-            print(f'Link: https://dragalialost.gamepedia.com/{character_name}')
-            await ctx.send(f'Link: https://dragalialost.gamepedia.com/{character_name}')
+        test_url = base_url + character_name
+        logger.info(f"Testing URL: {test_url}")
+        if url_validator(test_url) is not False:
+            await ctx.send(test_url)
+            logger.info(f"Successfully sent {test_url}")
         else:
-            print('Not found')
+            await ctx.send("Could not find")
+            logger.debug(f"Could not identify {test_url}")
--- saybot.py
+++ saybot.py
@@ -3,68 +3,34 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-import unsync
-import asyncio
-import logging
-import re
-import sys
-from typing import IO
-import urllib.error
-import urllib.parse
-import aiofiles
-import aiohttp
-from aiohttp import ClientSession
-from colorama import Fore
+from loguru import logger
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
-# TODO: Identify if cogs are being loaded once or not
+
 extensions = [
-    "Cogs.AdminCog.admin"
-    # "Cogs.DragaliaCog.dragalia" seems to be pulling the URL multiple times? Check this in a bit
+    "Cogs.AdminCog.admin",
+    "Cogs.DragaliaCog.dragalia"
 ]
 
-# TODO: This currently logs to the console which is fine, but needs to be more customized and log to a file
-logging.basicConfig(
-    format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
-    level=logging.DEBUG,
-    datefmt=Fore.YELLOW + "%H:%M:%S",
-    stream=sys.stderr,
-)
-logger = logging.getLogger("areq")
-logging.getLogger("chardet.charsetprober").disabled = True
+logger.add("Logging/discord.log")
 
 
-# TODO: Create Saybot, still needs help command, case info,
-bot = commands.AutoShardedBot(command_prefix=commands.when_mentioned_or('!'))
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
 
 
-# change presence
-# on guild join
-# on guild leave
-# guild metrics (in / members?)
+@client.event
+async def on_ready():
+    logger.info("Saybot is ready.")
 
-# TODO: Make this a debug only level command
-@commands.command(name='ShutdownBot')
-@commands.has_permissions(manage_roles=True)
-async def shutdown_bot(ctx):
-    '''
-    This is for debug purposes only and will need to be removed later
-    :param ctx:
-    :return:
-    '''
-    print(Fore.BLUE + 'Attempting a safe bot shutdown')
-    await ctx.send('Attempting to safe shutdown the bot')
-    sys.exit(1)
-
-bot.add_command(shutdown_bot)
 
 if __name__ == "__main__":
     for extension in extensions:
         try:
             bot.load_extension(extension)
+            logger.info(f"Loaded extension: {extension}")
         except Exception as error:
-            print(Fore.RED + f'Failed to load extension: {extension}')
-
+            logger.warning(f"Failed to load extension {extension}")
+    # bot.run is blocking, no code below this will run.
     bot.run(token)
